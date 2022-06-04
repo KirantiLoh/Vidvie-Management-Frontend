315fa40d293cb2da77bbe48c45ccf25077561d0a
Maurice Yang <mauriceyangmy28@gmail.com>
2022-06-04 11:31:20 +0700

    Moved roll call feature into another project

--- components/RollCall/RollCall.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RollCall = () => {
-  return (
-    <div>RollCall</div>
-  )
-}
-
-export default RollCall
--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,7 +1,6 @@
 import Title from '@components/Title'
 import { AuthContext } from '@context/AuthContext'
 import { withProtected } from '@hoc/route'
-import Link from 'next/link'
 import React, { useContext } from 'react'
 
 const AccountPage = () => {
@@ -14,10 +13,6 @@ const AccountPage = () => {
       <p>Username : {user.name}</p>
       <p>Division : {user.division}</p>
       <p>{user.leader_of ? `Leader of ${user.leader_of}` : null}</p>
-      <Link href='/account/roll-call'>
-        <a className='primary-btn' style={{display: 'inline-block'}}>Roll Call</a>
-      </Link>
-
     </div>
   )
 }
